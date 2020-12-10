1e1029571d27e0e06014dab0193d8866f22dd236
gomtangman <lbh0607@kaist.ac.kr>
2020-12-10 10:41:32 +0900

    volume 1 to 1.0

--- week0/venv/Scripts/activate.bat
+++ week0/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ShCho\Desktop\jungle_week0\week0\venv
+set VIRTUAL_ENV=C:\Users\Ben\Desktop\jungle_week0\week0\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
